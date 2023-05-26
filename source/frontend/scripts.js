a34cce768b32acb61236c033c01a3898a0dd2c9f
Daniel Lin <84496230+daniellin162@users.noreply.github.com>
2023-05-26 14:13:23 -0700

    Add files via upload

---
+++ source/frontend/scripts.js
@@ -0,0 +1,27 @@
+// When the user clicks on the button, toggle between hiding and showing the dropdown content 
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+//Generates results onClick
+function generate() {
+    let image = document.getElementById("display-img");
+    image.src = "./images/image1.svg";
+
+    let meaning = document.getElementById("meaning");
+    meaning.classList.toggle("show");
+}
