3075e3c1126530777372d358fc808db00c3902c3
jane814 <63211137+jane814@users.noreply.github.com>
2023-05-31 15:49:19 -0700

    added basic unit tests for save and reading functions

--- source/__tests__/localStorage.test.js
+++ source/__tests__/localStorage.test.js
@@ -1,9 +1,41 @@
 import '../scripts.js'
 
-//test to make sure readings array is saved properly onto localStorage
-let readings = generateReading('How can I create more balance in my friendships?');
-test('reading is saved on localStorage', () => {
+//basic test to make sure readings array is saved properly onto localStorage
+test('readings are saved on localStorage', () => {
+    let readings = [];
     saveReadings(readings);
-    let retrieved = localStorage.getItem('readings);
+    let retrieved = JSON.parse(localStorage.getItem('readings));
     expect(retrieved).toBe(readings);
 });
+
+//basic test to make sure that the saved array of readings is being retrieved correctly
+test('readings are retrieved correctly from localStorage', () => {
+    let readings = [];
+    saveReadings(readings);
+    let retrieved = getReadings();
+    expect(retrieved).toBe(readings);
+});
+
+//basic test to make sure that a reading is being saved correctly
+test('reading is saved on localStorage', () => {
+    let readings = [];
+    saveReadings(readings);
+    let reading = generateReading('How can I create more balance in my friendships?');
+    let key = reading.id;
+    saveReading(reading);
+    let retrieved = JSON.parse(localStorage.getItem('readings));
+    retrieved = readings[0];
+    expect(retrieved).toBe(reading);
+});
+
+//basic test to make sure that a reading is being retrieved correctly
+test('reading is saved on localStorage', () => {
+    let readings = [];
+    saveReadings(readings);
+    let reading = generateReading('How can I create more balance in my friendships?');
+    let key = reading.id;
+    saveReading(reading);
+    let retrieved = getReading(key);
+    expect(retrieved).toBe(reading);
+});
+
