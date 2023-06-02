70ec7670843898efd60cec644f8c44f43a238818
Preston Ellis <prestone03@gmail.com>
2023-06-01 18:18:53 -0700

    Lint all files

--- source/__tests__/localStorage.test.js
+++ source/__tests__/localStorage.test.js
@@ -1,9 +1,9 @@
-import '../scripts.js'
+import * as scripts from '../scripts.js';
 
 // basic test to make sure readings array is saved properly onto localStorage
 test('readings are saved on localStorage', () => {
   const readings = [];
-  saveReadings(readings);
+  scripts.saveReadings(readings);
   const retrieved = JSON.parse(localStorage.getItem('readings'));
   expect(retrieved).toBe(readings);
 });
@@ -11,17 +11,17 @@ test('readings are saved on localStorage', () => {
 // basic test to make sure that the saved array of readings is being retrieved correctly
 test('readings are retrieved correctly from localStorage', () => {
   const readings = [];
-  saveReadings(readings);
-  const retrieved = getReadings();
+  scripts.saveReadings(readings);
+  const retrieved = scripts.getReadings();
   expect(retrieved).toBe(readings);
 });
 
 //basic test to make sure that a reading is being saved correctly
 test('reading is saved on localStorage', () => {
   const readings = [];
-  saveReadings(readings);
-  const reading = generateReading('How can I create more balance in my friendships?');
-  saveReading(reading);
+  scripts.saveReadings(readings);
+  const reading = scripts.generateReading('How can I create more balance in my friendships?');
+  scripts.saveReading(reading);
   let retrieved = JSON.parse(localStorage.getItem('readings'));
   retrieved = retrieved[0];
   expect(retrieved).toBe(reading);
@@ -30,11 +30,11 @@ test('reading is saved on localStorage', () => {
 // basic test to make sure that a reading is being retrieved correctly
 test('reading is saved on localStorage', () => {
   const readings = [];
-  saveReadings(readings);
-  const reading = generateReading('How can I create more balance in my friendships?');
+  scripts.saveReadings(readings);
+  const reading = scripts.generateReading('How can I create more balance in my friendships?');
   const key = reading.id;
-  saveReading(reading);
-  const retrieved = getReading(key);
+  scripts.saveReading(reading);
+  const retrieved = scripts.getReading(key);
   expect(retrieved).toBe(reading);
 });
 
