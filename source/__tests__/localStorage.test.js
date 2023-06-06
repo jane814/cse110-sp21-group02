411647f06b97fb162090eba6b3204587627afd74
jane814 <63211137+jane814@users.noreply.github.com>
2023-06-06 00:41:47 -0700

    changed so that all tests are successful

--- source/__tests__/localStorage.test.js
+++ source/__tests__/localStorage.test.js
@@ -1,9 +1,10 @@
-const scripts = require('../scripts');
-
+const functions = require('../scripts.js');
 // basic test to make sure readings array is saved properly onto localStorage
 test('readings are saved on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
+  readings[0] = reading;
+  functions.saveReadings(readings);
   const retrieved = JSON.parse(localStorage.getItem('readings'));
   expect(retrieved).toEqual(readings);
 });
@@ -11,68 +12,67 @@ test('readings are saved on localStorage', () => {
 // basic test to make sure that the saved array of readings is being retrieved correctly
 test('readings are retrieved correctly from localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const retrieved = scripts.getReadings();
-  expect(retrieved).toBe(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
+  readings[0] = reading;
+  functions.saveReadings(readings);
+  const retrieved = functions.getReadings();
+  expect(retrieved).toEqual(readings);
 });
 
 //basic test to make sure that a reading is being saved correctly
 test('reading is saved on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const reading = scripts.generateReading('How can I create more balance in my friendships?');
-  scripts.saveReading(reading);
+  functions.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
+  functions.saveReading(reading);
   let retrieved = JSON.parse(localStorage.getItem('readings'));
   retrieved = retrieved[0];
-  expect(retrieved).toBe(reading);
+  expect(retrieved).toEqual(reading);
 });
 
 // basic test to make sure that a reading is being retrieved correctly
 test('reading is saved on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const reading = scripts.generateReading('How can I create more balance in my friendships?');
+  functions.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
   const key = reading.id;
-  scripts.saveReading(reading);
-  const retrieved = scripts.getReading(key);
-  expect(retrieved).toBe(reading);
+  functions.saveReading(reading);
+  const retrieved = functions.getReading(key);
+  expect(retrieved).toEqual(reading);
 });
 
 // basic test to make sure that a reading is removed properly
 test('reading is deleted on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const reading = scripts.generateReading('How can I create more balance in my friendships?');
+  functions.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
   const key = reading.id;
-  scripts.saveReading(reading);
-  scripts.deleteReading(key);
+  functions.saveReading(reading);
+  functions.deleteReading(key);
   const retrieved = JSON.parse(localStorage.getItem('readings'));
-  expect(retrieved).toBe([]);
+  expect(retrieved).toEqual([]);
 });
 
 // basic test to make sure that all readings are removed properly
 test('readings are deleted on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const reading = scripts.generateReading('How can I create more balance in my friendships?');
+  functions.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
   const key = reading.id;
-  scripts.saveReading(reading);
-  scripts.deleteReadings();
-  const retrieved = JSON.parse(localStorage.getItem('readings'));
-  expect(retrieved).toBe([]);
+  functions.saveReading(reading);
+  functions.deleteAllReadings();
+  const retrieved = localStorage.getItem('readings');
+  expect(retrieved).toEqual("");
 });
 
 // basic test to make sure that a reading is renamed properly
 test('reading is renamed on localStorage', () => {
   const readings = [];
-  scripts.saveReadings(readings);
-  const reading = scripts.generateReading('How can I create more balance in my friendships?');
+  functions.saveReadings(readings);
+  const reading = functions.generateReading('How can I create more balance in my friendships?');
   const key = reading.id;
-  scripts.saveReading(reading);
-  scripts.renameReading('test1',key);
-  const retrieved = JSON.parse(scripts.getReading(key));
-  expect(retrieved.name).toBe('test1');
+  functions.saveReading(reading);
+  functions.renameReading('test1',key);
+  const retrieved = functions.getReading(key);
+  expect(retrieved.name).toEqual('test1');
 });
-
-
-
